91d8dfc9f0b8fc2567230f90dba3bb3e93c4ac07
lanyeeee <lanyeeeeee@gmail.com>
2025-06-25 05:21:53 +0800

    feat: search suggestion

--- src/panes/SearchPane.vue
+++ src/panes/SearchPane.vue
@@ -1,7 +1,7 @@
 <script setup lang="tsx">
-import { ref, watch } from 'vue'
-import { commands } from '../bindings.ts'
-import { useMessage, useNotification } from 'naive-ui'
+import { computed, nextTick, ref, watch } from 'vue'
+import { commands, Suggestion } from '../bindings.ts'
+import { SelectOption, useMessage, useNotification } from 'naive-ui'
 import ComicCard from '../components/ComicCard.vue'
 import { useStore } from '../store.ts'
 import { useI18n } from '../utils.ts'
@@ -20,6 +20,15 @@ const searchInputRef = ref<InstanceType<typeof FloatLabelInput>>()
 const comicIdInput = ref<string>('')
 const currentPage = ref<number>(1)
 const comicCardContainerRef = ref<HTMLElement>()
+const {
+  onSuggestionSelect,
+  getSearchSuggestions,
+  suggestionOptions,
+  suggestionsShowing,
+  dropdownFocused,
+  dropdownX,
+  dropdownY,
+} = useSuggestion()
 
 const searching = ref<boolean>(false)
 
@@ -42,6 +51,7 @@ async function search(query: string, pageNum: number) {
   currentPage.value = pageNum
   store.currentTabName = 'search'
   searching.value = true
+  suggestionsShowing.value = false
 
   // TODO: support sort by popularity
   const result = await commands.search(query, pageNum, false)
@@ -107,6 +117,114 @@ async function pickComic() {
   store.currentTabName = 'comic'
 }
 
+function handleSearchInputKeydown(e: KeyboardEvent) {
+  if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+    dropdownFocused.value = suggestionsShowing.value
+  } else if (e.key === 'Enter') {
+    if (!dropdownFocused.value) {
+      search(searchInput.value.trim(), 1)
+    }
+  } else if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+    e.stopPropagation()
+  }
+}
+
+function handleSearchInputKeyup(e: KeyboardEvent) {
+  const { key } = e
+  if (key !== 'Enter' && key !== 'ArrowUp' && key !== 'ArrowDown' && key !== 'ArrowLeft' && key !== 'ArrowRight') {
+    getSearchSuggestions()
+  }
+}
+
+function useSuggestion() {
+  const suggestionsShowing = ref<boolean>(false)
+  const suggestions = ref<Suggestion[]>([])
+  const dropdownFocused = ref<boolean>(false)
+
+  const dropdownX = computed<number>(() => {
+    const x = searchInputRef.value?.NInputRef?.inputElRef?.getBoundingClientRect().left
+    return x ? x - 2 : 0
+  })
+
+  const dropdownY = computed<number>(() => {
+    const y = searchInputRef.value?.NInputRef?.inputElRef?.getBoundingClientRect().bottom
+    return y ? y - 2 : 0
+  })
+
+  watch(suggestionsShowing, (showing) => {
+    if (!showing) {
+      dropdownFocused.value = false
+    }
+  })
+
+  const suggestionOptions = computed<SelectOption[]>(() => {
+    return suggestions.value.map((suggestion) => {
+      const { s, n, t } = suggestion
+      return {
+        label: () => (
+          <div class="flex">
+            <span class="font-bold">{s}</span>
+            <span class="text-gray-5 px-1">({n})</span>
+            <span class="ml-auto text-gray-5">{t}</span>
+          </div>
+        ),
+        key: `${n}:${s.replace(' ', '_')}`,
+      }
+    })
+  })
+
+  async function getSearchSuggestions() {
+    suggestionsShowing.value = false
+    let lastWord = searchInput.value.split(' ').pop()
+    if (lastWord === undefined || lastWord === '') {
+      suggestions.value = []
+      return
+    }
+
+    if (lastWord.startsWith('-')) {
+      lastWord = lastWord.substring(1)
+    }
+
+    const result = await commands.getSearchSuggestions(lastWord)
+    if (result.status === 'error') {
+      console.error(result.error)
+      return
+    }
+
+    suggestions.value = result.data
+    suggestionsShowing.value = true
+  }
+
+  async function onSuggestionSelect(key: string) {
+    const words = searchInput.value.split(' ')
+    const isNegative = words.pop()?.startsWith('-')
+    if (isNegative === true) {
+      words.push(`-${key}`)
+    } else {
+      words.push(key)
+    }
+
+    searchInput.value = words.join(' ') + ' '
+    suggestionsShowing.value = false
+
+    await nextTick()
+    searchInputRef.value?.NInputRef?.scrollTo({
+      behavior: 'smooth',
+      left: 10000,
+    })
+  }
+
+  return {
+    suggestionsShowing,
+    suggestionOptions,
+    getSearchSuggestions,
+    onSuggestionSelect,
+    dropdownFocused,
+    dropdownX,
+    dropdownY,
+  }
+}
+
 defineExpose({ search })
 </script>
 
@@ -119,7 +237,17 @@ defineExpose({ search })
         size="small"
         v-model:value="searchInput"
         clearable
-        @keydown.enter="search(searchInput.trim(), 1)" />
+        @keyup="handleSearchInputKeyup"
+        @keydown="handleSearchInputKeydown" />
+      <n-dropdown
+        v-if="suggestionsShowing"
+        :x="dropdownX"
+        :y="dropdownY"
+        size="small"
+        :show="suggestionsShowing"
+        :options="suggestionOptions"
+        @select="onSuggestionSelect"
+        @clickoutside="() => (suggestionsShowing = false)" />
       <n-button :loading="searching" type="primary" class="w-15%" size="small" @click="search(searchInput.trim(), 1)">
         <template #icon>
           <n-icon size="22">
